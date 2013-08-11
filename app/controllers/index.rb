95fb0b796d3a1584d5825b9643559c08d85181d0
ltlai <leslie.t.lai@gmail.com>
2013-08-11 16:43:58 -0700

    Add colors and more restful routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,24 +9,19 @@ post '/' do
   redirect '/'
 end
 
-get '/:id' do
+get '/:id/edit' do
   @note = Note.find(params[:id])
-  # @title = "Edit note ##{params[:id]}"
   erb :edit
 end
 
 put '/:id' do
   note = Note.find(params[:id])
-  # note.title = params[:title]
-  # note.content = params[:content]
   note.update_attributes(title: params[:title], content: params[:content])
-  # note.save
   redirect '/'
 end
 
 get '/:id/delete' do
   @note = Note.find(params[:id])
-  @title = "Confirm deletion of note ##{params[:id]}"
   erb :delete
 end
 
